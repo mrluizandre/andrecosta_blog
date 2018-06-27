c6b2b27d0585e9427e58e4eef4b50a29d35fd720
Andre Costa <mr.luizandre@gmail.com>
2018-06-27 14:01:46 -0300

    Improve general layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def active_class(path)
     'active' if current_page?(path)
   end
+
+  def minutes_of_read(text)
+    words = text.split.size
+    minutes = words / 150
+
+    if minutes < 1
+      'menos de 1 minuto de leitura'
+    else
+      "aproximadamente #{pluralize(minutes,'minuto','minutos')} de leitura"
+    end
+  end
 end
