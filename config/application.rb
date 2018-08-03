459f63c43cf3e55ae83c2e7d8e47e98839f9a0b2
Andre Costa <mr.luizandre@gmail.com>
2018-08-03 11:07:59 -0300

    Try to fix deploy error: Change application.rb settings

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module AndrecostaBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.default_locale = 'pt-BR'
+    config.time_zone = 'Brasilia'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -18,12 +20,9 @@ module AndrecostaBlog
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.available_locales = [:en, :"pt-BR"]
-    config.i18n.default_locale = :"pt-BR"
 
   end
 end
