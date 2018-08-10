d3c8ef2ceec6163708010e8be0040349c1ade206
Andre Costa <mr.luizandre@gmail.com>
2018-08-10 18:39:43 -0300

    Add friendly urls for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, PostImageUploader
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 
   validates_presence_of :title, :image, :body
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180808000227) do
+ActiveRecord::Schema.define(version: 20180810213549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,18 @@ ActiveRecord::Schema.define(version: 20180808000227) do
     t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
+  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -34,6 +46,8 @@ ActiveRecord::Schema.define(version: 20180808000227) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.text "abstract"
+    t.string "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
